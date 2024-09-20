fd40612787eb8c3e1f9728f43321bb38d118e42e
Hebert Merchan <merchanhebert@gmail.com>
2024-09-19 23:28:02 -0400

    Se agrego un nuevo componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListadoGenericoComponent } from './utilidades/listado-generico/listado-generico.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListadoGenericoComponent
   ],
   imports: [
     BrowserModule,
